919a2c777d35e8283f8f11a1d88d3cd6e659ead4
Guilherme firmino <gustavorocha.180909@gmail.com>
2026-03-30 15:35:01 -0300

    upgrade
    atividade modulo 2

---
+++ modulo_02/atividade_modulo_02_gustavo.py
@@ -0,0 +1,11 @@
+'''
+
+atividade_modulo_02_gustavo.py
+Crie um programa que pede o nome de usuário e exibe uma mensagem personalizada.
+
+'''
+
+# Solicita o nome do usuário
+nome_usuario = input("Por favor, digite seu nome: ")
+# Exibe uma mensagem personalizada
+print(f"Olá, {nome_usuario}! Seja bem-vindo ao seu novo cantinho 🕸!")
